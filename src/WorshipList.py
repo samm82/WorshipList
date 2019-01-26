124787237e67e42087d646c372ae5ace0a7929c2
Sam Crawford <sam.crawford82@gmail.com>
2019-01-25 21:39:19 -0500

    Improved comments throughout code

--- src/WorshipList.py
+++ src/WorshipList.py
@@ -32,6 +32,8 @@ def main():
 
     lineCount = 0
 
+    # Writes each song
+
     doc, lineCount = writeSong(doc, lineCount, "ThePassion", "D")
     doc, lineCount = writeSong(doc, lineCount, "Anointing", "B")
     doc, lineCount = writeSong(doc, lineCount, "OPraiseTheName", "B")
@@ -55,11 +57,17 @@ def writeSong(doc, lineCount, fileName, oldKey):
     lines = infile.readlines()
     infile.close()
 
+    # Converts key to uppercase
+
     key = oldKey[0].upper()
     if len(oldKey) > 1:
         key += oldKey[1]
 
+    # Gets list of notes from getNotes(key)
+
     noteList = getNotes(key)
+
+    # Adds page break if song will get cut off
 
     if (lineCount + len(lines)) > 16:
         p = doc.add_paragraph()
@@ -95,6 +103,8 @@ def writeSong(doc, lineCount, fileName, oldKey):
         tab_stops = p.paragraph_format.tab_stops
         tab_stop = tab_stops.add_tab_stop(Inches(1.58), WD_TAB_ALIGNMENT.LEFT)
 
+        # Adds section name
+
         if line[0][-1] == ":":
             p.add_run(line[0] + "\t")
             chordStart = 1
@@ -107,6 +117,8 @@ def writeSong(doc, lineCount, fileName, oldKey):
         # small = 2 -> last small size
 
         small = 0
+
+        # Adds all chords
 
         for chord in line[chordStart:]:
             if chord == "|":
@@ -132,11 +144,15 @@ def writeSong(doc, lineCount, fileName, oldKey):
             else:
                 run = p.add_run(getChord(noteList, chord, fileName) + "  ")
 
+            # Set font size for small text
+
             if small == 1:
                 run.font.size = Pt(22)
             if small == 2:
                 run.font.size = Pt(22)
                 small = 0
+
+        # Sets paragraph spacing
 
         p.paragraph_format.line_spacing = Pt(36)
         p.paragraph_format.space_after = Pt(0)
@@ -149,10 +165,13 @@ def writeSong(doc, lineCount, fileName, oldKey):
 #  @return        A list of notes in the given key.
 #  @throw         ParamError if the key isn't valid.
 def getNotes(key):
-    # pull this step out to getNotes()
+    # Defines standard lists
+
     validKeys  = ['F','F#','Gb','G','G#','Ab','A','A#','Bb','B','C','C#','Db','D','D#','Eb','E']
     notesSharp = ['F','F#','G','G#','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C','C#','D','D#','E']
     notesFlat  = ['F','Gb','G','Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B','C','Db','D','Eb','E']
+
+    # Checks if key is valid
 
     if key not in validKeys:
         raise ParamError("The key \"" + key + "\" isn't recognized.")
@@ -167,6 +186,8 @@ def getNotes(key):
     else:
         notes = notesSharp
 
+    # Gets notes of the major scale started at the given key
+
     noteList = []
     note = notes.index(key)
     for i in list(range(7)):
@@ -175,6 +196,7 @@ def getNotes(key):
             note += 1
         else:
             note += 2
+
     return noteList
 
 ## @brief              Gets chord from Roman numeral based on list of notes.
@@ -184,6 +206,8 @@ def getNotes(key):
 #  @return             The chord converted from the Roman numeral.
 #  @throw              FileError if the chord isn't valid.
 def getChord(noteList, num, fileName):
+    # Handles if key is minor
+
     minor = False
     if num.islower():
         minor = True
@@ -192,12 +216,16 @@ def getChord(noteList, num, fileName):
 
     # Finds chord from list based on numeral
     numList = ["i", "ii", "iii", "iv", "v", "vi", "vii"]
-    
+
+    # Checks if chord is valid, and retrieves it from list if it is
+
     if num not in numList:
         raise FileError("The chord \"" + num + "\" in the " + fileName + " file isn't recognized.")
     else:
         chord = noteList[numList.index(num)]
-        
+
+    # Finishes minor handling
+
     if minor:
         chord += "m"
 
