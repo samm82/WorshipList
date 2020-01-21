e1b67800351df12ad22955f2d3297b6d12c4d43f
Sam Crawford <sam.crawford82@gmail.com>
2020-01-21 12:06:14 -0500

    Improved popupError

--- src/GUI.py
+++ src/GUI.py
@@ -1,7 +1,7 @@
 ## @file   GUI.py
 #  @brief  Implements GUI for selecting songs.
 #  @author Samuel Crawford
-#  @date   1/17/2020
+#  @date   1/21/2020
 
 import PySimpleGUI as sg
 
@@ -118,15 +118,14 @@ def checkSongGUI(songs, keys):
 
 ## @brief            Defines an error popup that signifies incorrect input.
 #  @param[in] string The error string to be printed in dialogue box.
-#  @return           A (verified) list of songs and their keys.
 def popupError(string):
     errorDialgue = [
         [sg.Text(string)], 
         [sg.CloseButton("OK")]
     ]
 
-    songWindow = sg.Window("Error").Layout(errorDialgue)
-    button, values = songWindow.Read()
+    errorWindow = sg.Window("Error").Layout(errorDialgue)
+    errorWindow.Read()
 
 ## @brief  Implements GUI for retrieving the file name.
 #  @return The file name.
