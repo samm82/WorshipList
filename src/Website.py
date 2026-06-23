ccb0fe466f6da802b1e117cfce360a84f2966e06
Samuel Crawford <sam.crawford82@gmail.com>
2026-06-23 16:44:10 -0400

    Add first attempt at filename verification
    'Use Next Sunday' also submits form; hopefully adding its functionality fixes this

--- src/Website.py
+++ src/Website.py
@@ -2,17 +2,20 @@
 
 from flask import Flask, render_template, request
 
+from Helpers import checkFileName
+
 app = Flask(__name__, template_folder='website')
 
 @app.route("/")
-def welcome_page():
-    return render_template("main.html")
+def main_page():
+    return render_template("main.html", filenameVal="", validFileName=1)
 
-@app.route("/file2", methods=['POST'])
-def second_page():
-    html_data = request.form["enter_value"]
-    return render_template("file2.html", html_data=html_data)
-# request form from id in html file
+@app.route("/", methods=['POST'])
+def process_filename():
+    # request form from id in html file
+    filenameVal = request.form["filename"]
+    return render_template("main.html", filenameVal=filenameVal,
+                           validFileName=int(checkFileName(filenameVal)))
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
