9a83c1597230ccf22e9970142f3e1ad43c3fe324
Samuel Crawford <sam.crawford82@gmail.com>
2026-06-24 17:25:48 -0400

    Populate filename field with next Sunday on button press
    Change format of filename buttons
    Lint website Python code

--- src/Website.py
+++ src/Website.py
@@ -2,20 +2,22 @@
 
 from flask import Flask, render_template, request
 
-from Helpers import checkFileName
+from Helpers import checkFileName, useNextSunday
 
 app = Flask(__name__, template_folder='website')
+
 
 @app.route("/")
 def main_page():
     return render_template("main.html", filenameVal="", validFileName=1)
 
+
 @app.route("/", methods=['POST'])
 def process_filename():
-    # request form from id in html file
-    filenameVal = request.form["filename"]
+    filenameVal = useNextSunday() if "btn_Sun" in request.form else request.form["filename"]
     return render_template("main.html", filenameVal=filenameVal,
                            validFileName=int(checkFileName(filenameVal)))
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
